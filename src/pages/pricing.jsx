05fa4384530a3505eda06e6f7503959f48a39fff
Mark Lewis <22421135+marklewis01@users.noreply.github.com>
2018-06-05 23:41:20 +1000

    Pricing page cards styling complete

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -7,128 +7,257 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 
 import Container from '../components/Container'
 
 const styles = theme => ({
-  sectionLight: {
-    width: `100vw`,
+  card: {
+    margin: theme.spacing.unit * 1.5,
+    height: `100%`,
+    display: 'flex',
+    flexDirection: 'column',
   },
+  cardList: {
+    marginBottom: `1rem`,
+    marginTop: `1rem`,
+  },
+  cardListItem: {
+    paddingTop: 'unset',
+    paddingBottom: 'unset',
+  },
+  cardPrice: {
+    margin: `20px 0 5px`,
+    fontSize: `2rem`,
+    fontWeight: `bold`,
+    '& span': {
+      color: theme.palette.grey[400],
+      fontSize: `1.5rem`,
+      fontWeight: `300`,
+    },
+  },
+  cardTitle: {
+    margin: `20px 0 5px`,
+  },
+  cardWrapper: {
+    '@media (max-width: 960px)': {
+      marginBottom: `2rem`,
+    },
+  },
+  sectionLight: {},
   sectionDark: {
     backgroundColor: theme.palette.grey[300],
-    width: `100vw`,
   },
 })
 
 const PricingPage = ({ classes }) => (
   <Grid container>
-    <Grid item className={classes.sectionLight}>
+    <Grid container className={classes.sectionLight}>
       <Container>
         <Grid item xs={12}>
           <Typography variant="display2" align="center">
             Pricing
           </Typography>
-          <Typography variant="subheading" align="center">
-            Quickly build an effective pricing table for your potential
-            customers with this Bootstrap example. It's built with default
-            Bootstrap components and utilities with little customization.
-          </Typography>
+          <br />
+          <hr />
+          <Grid container justify="center">
+            <Grid item xs={8}>
+              <Typography variant="subheading" align="center">
+                Quickly build an effective pricing table for your potential
+                customers with this Bootstrap example. It's built with default
+                Bootstrap components and utilities with little customization.
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Container>
     </Grid>
-    <Grid container className={classes.sectionDark}>
+    <Grid container className={classes.sectionDark} justify="center">
       <Container flex>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardHeader
-              title="Free Tier"
-              subheader="Try us out for as long as you like"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="headline">
-                $0
-              </Typography>
-              <Typography gutterBottom variant="subheading">
-                /month
-              </Typography>
-              <Typography variant="body1">
-                <ul>
-                  <li>10 users included</li>
-                  <li>2 GB of storage</li>
-                  <li>Email support</li>
-                  <li>Help center access</li>
-                </ul>
-              </Typography>
-              <Button
-                variant="outlined"
-                color="primary"
-                className={classes.button}
-                fullWidth
+        <Grid item xs={12} md={4} className={classes.cardWrapper}>
+          <Paper className={classes.card}>
+            <Grid item>
+              <Typography
+                variant="headline"
+                align="center"
+                className={classes.cardTitle}
               >
-                Sign up for free
-              </Button>
-            </CardContent>
-          </Card>
+                Free Tier
+              </Typography>
+              <Typography variant="caption" align="center">
+                Try us out for as long as you like
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                gutterBottom
+                variant="headline"
+                align="center"
+                className={classes.cardPrice}
+              >
+                $0 <span>/month</span>
+              </Typography>
+              <List className={classes.cardList}>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="10 users included" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="2 GB of storage" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Email support" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Help center access" />
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid container justify="center">
+              <Grid item xs={10}>
+                <Button variant="outlined" color="primary" fullWidth>
+                  Sign up for free
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardHeader
-              title="Small Business"
-              subheader="Perfect for small teams of 2-5 users"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="headline">
-                $19
-              </Typography>
-              <Typography variant="body1">
-                <ul>
-                  <li>20 users included</li>
-                  <li>15 GB of storage</li>
-                  <li>Priority email support</li>
-                  <li>Help center access</li>
-                </ul>
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                fullWidth
+        <Grid item xs={12} md={4} className={classes.cardWrapper}>
+          <Paper className={classes.card}>
+            <Grid item>
+              <Typography
+                variant="headline"
+                align="center"
+                className={classes.cardTitle}
               >
-                Get started
-              </Button>
-            </CardContent>
-          </Card>
+                Small Business
+              </Typography>
+              <Typography variant="caption" align="center">
+                Perfect for small teams of 2-5 users
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                gutterBottom
+                variant="headline"
+                align="center"
+                className={classes.cardPrice}
+              >
+                $19 <span>/month</span>
+              </Typography>
+              <List className={classes.cardList}>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="20 users included" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="15 GB of storage" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Priority email support" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Help center access" />
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid container justify="center">
+              <Grid item xs={10}>
+                <Button variant="contained" color="primary" fullWidth>
+                  Get started
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardHeader
-              title="Corporate"
-              subheader="When compliance and reporting matter"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="headline">
-                $49
-              </Typography>
-              <Typography variant="body1">
-                <ul>
-                  <li>30 users included</li>
-                  <li>25 GB of storage</li>
-                  <li>Phone and email support</li>
-                  <li>Help center access</li>
-                </ul>
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                fullWidth
+        <Grid item xs={12} md={4} className={classes.cardWrapper}>
+          <Paper className={classes.card}>
+            <Grid item>
+              <Typography
+                variant="headline"
+                align="center"
+                className={classes.cardTitle}
               >
-                Contact us
-              </Button>
-            </CardContent>
-          </Card>
+                Corporate
+              </Typography>
+              <Typography variant="caption" align="center">
+                When compliance and reporting matter
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                gutterBottom
+                variant="headline"
+                align="center"
+                className={classes.cardPrice}
+              >
+                $49 <span>/month</span>
+              </Typography>
+              <List className={classes.cardList}>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="30 users included" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="25 GB of storage" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Phone and email support" />
+                </ListItem>
+                <ListItem className={classes.cardListItem}>
+                  <ListItemIcon>
+                    <CheckCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Help center access" />
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid container justify="center">
+              <Grid item xs={10}>
+                <Button variant="contained" color="primary" fullWidth>
+                  Contact us
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
       </Container>
     </Grid>
