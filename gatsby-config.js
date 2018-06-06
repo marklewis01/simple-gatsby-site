b6bbdd7124b605a1d5426b3c763eee4486c79fa3
Mark Lewis <22421135+marklewis01@users.noreply.github.com>
2018-06-06 12:17:19 +1000

    adding Google Analytics to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+import config from './private-config'
+
 module.exports = {
   pathPrefix: `/simple-gatsby-site`,
   siteMetadata: {
@@ -15,5 +17,19 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: config.gaTracking,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+      },
+    },
   ],
 }
